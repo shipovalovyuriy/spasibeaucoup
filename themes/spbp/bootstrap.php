3bbb79ba09dc430c741517ab1e56db94234cc4ca
Kairzhan <author_gang@mail.ru>
2016-02-28 18:49:48 +0600

    theme init

---
+++ themes/spbp/bootstrap.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Theme bootstrap file.
+ */
+// We need to do this to ensure that assets was published even if we don't use any booster widget on the page
+if (Yii::app()->hasComponent('bootstrap')) {
+    Yii::app()->getComponent('bootstrap');
+}
+// Get clientScript component
+$clientScript = Yii::app()->getClientScript();
+
+// папка с ресурсами темы (по умолчанию web) будет опубликована и возвращен публичный url для неё
+$assetManager = Yii::app()->getAssetManager();
+
+// Publish theme assets, note that we keep assets in web folder
+$assetPath = $assetManager->publish(
+    Yii::app()->theme->basePath . '/web'
+);
+$styles = array(
+    'bootstrap.css',
+    'style.css',
+    'demo.css',     
+    'generator.css',        
+);
+
+foreach ($styles as $style) {
+    $clientScript->registerCssFile($assetPath . '/css/' . $style);
+}
+
+// Javascript
+$scripts = array(
+    //'jquery.min.js' => CClientScript::POS_HEAD,
+    'bootstrap/js/bootstrap.min.js' => CClientScript::POS_END,
+    'modernizr.custom.js' => CClientScript::POS_END,
+    'device.min.js' => CClientScript::POS_END,
+    'snap.min.js' => CClientScript::POS_END,
+    'jquery.pagescroller.lite.js' => CClientScript::POS_END,
+    'plugins/revslider/js/jquery.themepunch.revolution.min.js' => CClientScript::POS_END,
+    'plugins/thumbscroller/jquery-ui-1.8.13.custom.min.js' => CClientScript::POS_END,
+    'plugins/thumbscroller/jquery.thumbnailScroller.js' => CClientScript::POS_END,
+    'plugins/revslider/js/jquery.themepunch.plugins.min.js' => CClientScript::POS_END,
+    'plugins/flexslider/jquery.flexslider-min.js' => CClientScript::POS_END,
+    'plugins/magnificpopup/jquery.magnific-popup.min.js' => CClientScript::POS_END,
+    'jquery.countTo.js' => CClientScript::POS_END,
+    'contact-form.js' => CClientScript::POS_END,
+    'singlePageNav.js' => CClientScript::POS_END,
+    'main.js' => CClientScript::POS_END,
+    'demo.js' => CClientScript::POS_END,
+);
+
+foreach ($scripts as $script => $position) {
+    $clientScript->registerScriptFile($assetPath . '/js/' . $script, $position);
+}
+
+
+$clientScript->registerPackage('jquery');
+$clientScript->registerLinkTag('shortcut icon', null, $assetPath . '/images/favicon.ico');
