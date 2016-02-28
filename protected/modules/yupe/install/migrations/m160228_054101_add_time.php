36dd4d350416e1a522d587d54fcd89334bb6c4f0
Kairzhan <author_gang@mail.ru>
2016-02-28 18:36:20 +0600

    migrations fix

--- protected/modules/yupe/install/migrations/m160228_054101_add_time.php
+++ protected/modules/yupe/install/migrations/m160228_054101_add_time.php
@@ -4,9 +4,9 @@ class m160228_054101_add_time extends yupe\components\DbMigration
 {
 	public function safeUp()
 	{
-            $this->addColumn('user_teacher', 'time', 'VARCHAR(50) NULL DEFAULT NULL');
-            $this->addColumn('listner_position', 'time', 'VARCHAR(255) NULL DEFAULT NULL');
-            $this->addColumn('listner_schedule', 'time', 'VARCHAR(50) NULL DEFAULT NULL');
+            $this->addColumn('{{user_teacher}}', 'time', 'VARCHAR(50) NULL DEFAULT NULL');
+            $this->addColumn('{{listner_position}}', 'time', 'VARCHAR(255) NULL DEFAULT NULL');
+            $this->addColumn('{{listner_schedule}}', 'time', 'VARCHAR(50) NULL DEFAULT NULL');
 	}
 
 	public function safeDown()
