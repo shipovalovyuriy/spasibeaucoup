972ebea9c37a781d3862d45db9cc15dcf6a32509
Kairzhan <author_gang@mail.ru>
2016-02-29 12:24:19 +0600

    migrate fix

--- protected/modules/yupe/install/migrations/m160228_202329_hui.php
+++ protected/modules/yupe/install/migrations/m160228_202329_hui.php
@@ -3,8 +3,9 @@
 class m160228_202329_hui extends yupe\components\DbMigration
 {
 	public function safeUp()
-	{
-		$this->addForeignKey('FK_roomID','{{listner_schedule}}','room_id','{{branch_room}}','id');
+	{   
+            $this->addColumn('{{listner_schedule}}', 'room_id', 'INT(11)');
+            $this->addForeignKey('FK_roomID','{{listner_schedule}}','room_id','{{branch_room}}','id');
 	}
 
 	public function safeDown()
