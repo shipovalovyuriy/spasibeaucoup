f8cec5ae4d70455aca74cd4b439319f24fbeed38
Kairzhan <author_gang@mail.ru>
2016-03-13 21:57:41 +0600

    outflow crud

--- protected/modules/balance/models/Outflow.php
+++ protected/modules/balance/models/Outflow.php
@@ -15,6 +15,7 @@
  * The followings are the available model relations:
  * @property BalanceCost $cost
  */
+Yii::import('application.modules.balance.models.Cost');
 class Outflow extends yupe\models\YModel
 {
 	/**
@@ -33,22 +34,21 @@ class Outflow extends yupe\models\YModel
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-<<<<<<< HEAD
-			array('receiver, price, based, note', 'required'),
+                        array('receiver, price, based, note', 'required'),
 			array('cost_id', 'numerical', 'integerOnly'=>true),
 			array('receiver, price, based, note', 'length', 'max'=>50),
 			array('date', 'safe'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
 			array('id, cost_id, receiver, date, price, based, note', 'safe', 'on'=>'search'),
-=======
+
 			array('cost_id', 'numerical', 'integerOnly'=>true),
 			array('receiver, date, based, comment', 'length', 'max'=>50),
 			array('cost', 'length', 'max'=>20),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
 			array('id, cost_id, receiver, date, cost, based, comment', 'safe', 'on'=>'search'),
->>>>>>> origin/master
+
 		);
 	}
 
@@ -60,11 +60,7 @@ class Outflow extends yupe\models\YModel
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-<<<<<<< HEAD
-			'cost' => array(self::BELONGS_TO, 'BalanceCost', 'cost_id'),
-=======
-			'costs' => array(self::BELONGS_TO, 'Cost', 'cost_id'),
->>>>>>> origin/master
+			'cost' => array(self::BELONGS_TO, 'Cost', 'cost_id'),
 		);
 	}
 
@@ -75,11 +71,7 @@ class Outflow extends yupe\models\YModel
 	{
 		return array(
 			'id' => 'ID',
-<<<<<<< HEAD
 			'cost_id' => 'Cost',
-=======
-			'cost_id' => 'Costs',
->>>>>>> origin/master
 			'receiver' => 'Receiver',
 			'date' => 'Date',
 			'price' => 'Price',
---
+++ protected/modules/balance/views/outflowBackend/_form.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Отображение для _form:
+ *
+ *   @category YupeView
+ *   @package  yupe
+ *   @author   Yupe Team <team@yupe.ru>
+ *   @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
+ *   @link     http://yupe.ru
+ *
+ *   @var $model Outflow
+ *   @var $form TbActiveForm
+ *   @var $this OutflowBackendController
+ **/
+$form = $this->beginWidget(
+    'bootstrap.widgets.TbActiveForm', [
+        'id'                     => 'outflow-form',
+        'enableAjaxValidation'   => false,
+        'enableClientValidation' => true,
+        'htmlOptions'            => ['class' => 'well'],
+    ]
+);
+?>
+
+<div class="alert alert-info">
+    <?php echo Yii::t('BalanceModule.balance', 'Поля, отмеченные'); ?>
+    <span class="required">*</span>
+    <?php echo Yii::t('BalanceModule.balance', 'обязательны.'); ?>
+</div>
+
+<?php echo $form->errorSummary($model); ?>
+
+    <div class="row">
+        <div class="col-sm-7">
+            <?php echo $form->dropDownListGroup($model, 'cost_id', [
+                    'widgetOptions' => [
+                        'data' => CHtml::listData(Cost::model()->findAll(), 'id', 'name')
+                    ]
+                ]); ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-7">
+            <?php echo $form->textFieldGroup($model, 'receiver', [
+                'widgetOptions' => [
+                    'htmlOptions' => [
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('receiver'),
+                        'data-content' => $model->getAttributeDescription('receiver')
+                    ]
+                ]
+            ]); ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-7">
+            <?php echo $form->textFieldGroup($model, 'date', [
+                'widgetOptions' => [
+                    'htmlOptions' => [
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('date'),
+                        'data-content' => $model->getAttributeDescription('date')
+                    ]
+                ]
+            ]); ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-7">
+            <?php echo $form->textFieldGroup($model, 'price', [
+                'widgetOptions' => [
+                    'htmlOptions' => [
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('price'),
+                        'data-content' => $model->getAttributeDescription('price')
+                    ]
+                ]
+            ]); ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-7">
+            <?php echo $form->textFieldGroup($model, 'based', [
+                'widgetOptions' => [
+                    'htmlOptions' => [
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('based'),
+                        'data-content' => $model->getAttributeDescription('based')
+                    ]
+                ]
+            ]); ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-7">
+            <?php echo $form->textFieldGroup($model, 'note', [
+                'widgetOptions' => [
+                    'htmlOptions' => [
+                        'class' => 'popover-help',
+                        'data-original-title' => $model->getAttributeLabel('note'),
+                        'data-content' => $model->getAttributeDescription('note')
+                    ]
+                ]
+            ]); ?>
+        </div>
+    </div>
+
+    <?php $this->widget(
+        'bootstrap.widgets.TbButton', [
+            'buttonType' => 'submit',
+            'context'    => 'primary',
+            'label'      => Yii::t('BalanceModule.balance', 'Сохранить Расход и продолжить'),
+        ]
+    ); ?>
+    <?php $this->widget(
+        'bootstrap.widgets.TbButton', [
+            'buttonType' => 'submit',
+            'htmlOptions'=> ['name' => 'submit-type', 'value' => 'index'],
+            'label'      => Yii::t('BalanceModule.balance', 'Сохранить Расход и закрыть'),
+        ]
+    ); ?>
+
+<?php $this->endWidget(); ?>
