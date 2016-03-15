c540a4859f758bbae2fb73b955a50b41d2b85476
shipovalovyuriy <shipovalovyuriy@gmail.com>
2016-03-15 16:49:42 +0600

    gavno

--- protected/modules/listner/views/position/_form.php
+++ protected/modules/listner/views/position/_form.php
@@ -2,22 +2,22 @@
 /**
  * Отображение для _form:
  *
- *   @category YupeView
- *   @package  yupe
- *   @author   Yupe Team <team@yupe.ru>
- *   @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
- *   @link     http://yupe.ru
+ * @category YupeView
+ * @package  yupe
+ * @author   Yupe Team <team@yupe.ru>
+ * @license  https://github.com/yupe/yupe/blob/master/LICENSE BSD
+ * @link     http://yupe.ru
  *
- *   @var $model Position
- *   @var $form TbActiveForm
- *   @var $this PositionController
+ * @var $model Position
+ * @var $form TbActiveForm
+ * @var $this PositionBackendController
  **/
 $form = $this->beginWidget(
     'bootstrap.widgets.TbActiveForm', [
-        'id'                     => 'position-form',
-        'enableAjaxValidation'   => false,
+        'id' => 'position-form',
+        'enableAjaxValidation' => false,
         'enableClientValidation' => true,
-        'htmlOptions'            => ['class' => 'well'],
+        'htmlOptions' => ['class' => 'well'],
     ]
 );
 ?>
@@ -30,122 +30,170 @@ $form = $this->beginWidget(
 
 <?php echo $form->errorSummary($model); ?>
 
-    <div class="row">
-        <div class="col-sm-7">
-            <?php echo $form->textFieldGroup($model, 'code', [
-                'widgetOptions' => [
-                    'htmlOptions' => [
-                        'class' => 'popover-help',
-                        'data-original-title' => $model->getAttributeLabel('code'),
-                        'data-content' => $model->getAttributeDescription('code')
-                    ]
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->textFieldGroup($model, 'code', [
+            'widgetOptions' => [
+                'htmlOptions' => [
+                    'class' => 'popover-help',
+                    'data-original-title' => $model->getAttributeLabel('code'),
+                    'data-content' => $model->getAttributeDescription('code')
                 ]
-            ]); ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-7">
-            <?php echo $form->dropDownListGroup($model, 'form_id', [
-                    'widgetOptions' => [
-                        'data' => CHtml::listData(Form::model()->findAll(), 'id', 'name')
-                    ]
-                ]); ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-7">
-            <?php echo $form->dropDownListGroup($model, 'teacher_id', [
-                    'widgetOptions' => [
-                        'data' => CHtml::listData(Teacher::model()->findAll(), 'id', 'user.first_name')
-                    ]
-                ]); ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-7">
-            <?php echo $form->dropDownListGroup($model, 'subject_id', [
-                    'widgetOptions' => [
-                        'data' => CHtml::listData(Subject::model()->findAll(), 'id', 'name')
-                    ]
-                ]); ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-7">
-            <?php echo $form->dropDownListGroup($model, 'group_id', [
-                    'widgetOptions' => [
-                        'data' => CHtml::listData(Group::model()->findAll(), 'id', 'name')
-                    ]
-                ]); ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-7">
-            <?php echo $form->textFieldGroup($model, 'lvl', [
-                'widgetOptions' => [
-                    'htmlOptions' => [
-                        'class' => 'popover-help',
-                        'data-original-title' => $model->getAttributeLabel('lvl'),
-                        'data-content' => $model->getAttributeDescription('lvl')
-                    ]
-                ]
-            ]); ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-7">
-            <?php echo $form->textFieldGroup($model, 'note', [
-                'widgetOptions' => [
-                    'htmlOptions' => [
-                        'class' => 'popover-help',
-                        'data-original-title' => $model->getAttributeLabel('note'),
-                        'data-content' => $model->getAttributeDescription('note')
-                    ]
-                ]
-            ]); ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-7">
-            <?php echo $form->textFieldGroup($model, 'time', [
-                'widgetOptions' => [
-                    'htmlOptions' => [
-                        'class' => 'popover-help',
-                        'data-original-title' => $model->getAttributeLabel('time'),
-                        'data-content' => $model->getAttributeDescription('time')
-                    ]
-                ]
-            ]); ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-7">
-            <?php echo $form->datePickerGroup($model,'start_date', [
-            'widgetOptions'=>[
-                'options' => [],
-                'htmlOptions' => []
-            ],
-            'prepend'=>'<i class="fa fa-calendar"></i>'
+            ]
         ]); ?>
-        </div>
     </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->dropDownListGroup($model, 'form_id', [
+            'widgetOptions' => [
+                'data' => CHtml::listData(Form::model()->findAll(), 'id', 'name')
+            ]
+        ]); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->dropDownListGroup($model, 'listner_id', [
+            'widgetOptions' => [
+                'data' => CHtml::listData(Listner::model()->findAll(), 'id', 'name')
+            ]
+        ]); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->dropDownListGroup($model, 'teacher_id', [
+            'widgetOptions' => [
+                'data' => CHtml::listData(Teacher::model()->findAll(), 'id', 'id')
+            ]
+        ]); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->dropDownListGroup($model, 'subject_id', [
+            'widgetOptions' => [
+                'data' => CHtml::listData(Subject::model()->findAll(), 'id', 'name')
+            ]
+        ]); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->dropDownListGroup($model, 'group_id', [
+            'widgetOptions' => [
+                'data' => CHtml::listData(Group::model()->findAll(), 'id', 'name')
+            ]
+        ]); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->textFieldGroup($model, 'lvl', [
+            'widgetOptions' => [
+                'htmlOptions' => [
+                    'class' => 'popover-help',
+                    'data-original-title' => $model->getAttributeLabel('lvl'),
+                    'data-content' => $model->getAttributeDescription('lvl')
+                ]
+            ]
+        ]); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <?php echo $form->textFieldGroup($model, 'note', [
+            'widgetOptions' => [
+                'htmlOptions' => [
+                    'class' => 'popover-help',
+                    'data-original-title' => $model->getAttributeLabel('note'),
+                    'data-content' => $model->getAttributeDescription('note')
+                ]
+            ]
+        ]); ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-7">
+        <!--            --><?php //echo $form->textFieldGroup($model, 'time', [
+        //                'widgetOptions' => [
+        //                    'htmlOptions' => [
+        //                        'class' => 'popover-help',
+        //                        'data-original-title' => $model->getAttributeLabel('time'),
+        //                        'data-content' => $model->getAttributeDescription('time')
+        //                    ]
+        //                ]
+        //            ]); ?>
+        <p><label for="">Добавить время занятий</label></p>
+        <?php
+        $this->widget('application.modules.listner.widgets.timepicker.timepicker', array(
+            'model' => $model,
+            'name' => 'time',
+            'select' => 'time',
+        ));
+        ?>
+        <a id="addTime" href="javascript:void()" style="text-decoration:none; cursor:pointer;"
+           class="glyphicon glyphicon-plus"></a>
 
-    <?php $this->widget(
-        'bootstrap.widgets.TbButton', [
-            'buttonType' => 'submit',
-            'context'    => 'primary',
-            'label'      => Yii::t('ListnerModule.listner', 'Сохранить Положение и продолжить'),
-        ]
-    ); ?>
-    <?php $this->widget(
-        'bootstrap.widgets.TbButton', [
-            'buttonType' => 'submit',
-            'htmlOptions'=> ['name' => 'submit-type', 'value' => 'index'],
-            'label'      => Yii::t('ListnerModule.listner', 'Сохранить Положение и закрыть'),
-        ]
-    ); ?>
+
+        <p><label for="">Итоговое время:</label></p>
+        <input class="totalTime" value="<?=$model->time ?>" type="text" name="Position[time]"> <a href="javascript:void()" id="clearTime"
+                                                                                                  style="cursor:pointer;">Очистить</a>
+    </div>
+</div>
+<div class="row" style="margin-bottom:30px;">
+    <div class="col-sm-7">
+        <p><label for="">Дата начала</label></p>
+        <?php
+        $this->widget('application.modules.listner.widgets.timepicker.timepicker', array(
+            'model' => $model,
+            'name' => 'start_date',
+            'select' => 'date',
+        ));
+        ?>
+    </div>
+</div>
+
+<?php $this->widget(
+    'bootstrap.widgets.TbButton', [
+        'buttonType' => 'submit',
+        'context' => 'primary',
+        'label' => Yii::t('ListnerModule.listner', 'Сохранить Положение и продолжить'),
+    ]
+); ?>
+<?php $this->widget(
+    'bootstrap.widgets.TbButton', [
+        'buttonType' => 'submit',
+        'htmlOptions' => ['name' => 'submit-type', 'value' => 'index'],
+        'label' => Yii::t('ListnerModule.listner', 'Сохранить Положение и закрыть'),
+    ]
+); ?>
 
 <?php $this->endWidget(); ?>
+
 <script>
-    
+
+
+    var el = document.getElementById("addTime");
+    var currentTime = document.getElementById("yw0");
+    var total = document.getElementsByClassName("totalTime");
+    el.addEventListener("click", function () {
+
+        //console.log(currentTime.value);
+        if (total.value == undefined) {
+            total.value = currentTime.value
+        } else {
+            total.value += "," + currentTime.value;
+        }
+        $('.totalTime').val(total.value);
+    });
+
+    $('#clearTime').click(function () {
+        $('.totalTime').val('');
+    })
+
+    $()
+
 </script>
+
